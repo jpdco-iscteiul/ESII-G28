1d4c0223539618d3692b7dbba90e3415f793cc83
jpdco-iscteiul <jpdco@iscte-iul.pt>
2020-03-26 17:22:04 +0000

    Testes

--- HelloWorld/src/test/java/ES1_2019_C2_66/HelloWorld/AppTest.java
+++ HelloWorld/src/test/java/ES1_2019_C2_66/HelloWorld/AppTest.java
@@ -1,8 +1,10 @@
 package ES1_2019_C2_66.HelloWorld;
 
+import org.junit.Assert;
+import org.junit.Before;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
@@ -10,31 +12,50 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+private App test;
+	
+	@Before
+	public void setUp() throws Exception{
+		test = new App();
+	}
+	
+	
+	public void test() {
+		Assert.assertFalse(test.containString("Filarmonica", "pastel"));
+		Assert.assertTrue(test.containString("Filarmonica", "monica"));
+		
+		Assert.assertEquals(0, test.comparatorInt(3, 3));
+		Assert.assertEquals(1, test.comparatorInt(4, 3));
+		Assert.assertEquals(-1, test.comparatorInt(3, 4));
+		
+		double result =28.27;
+		Assert.assertEquals(result, test.circleArea(3));
+	}
 }
 	
 	
--- HelloWorld/src/test/java/ES1_2019_C2_66/HelloWorld/Tests.java
+++ HelloWorld/src/test/java/ES1_2019_C2_66/HelloWorld/Tests.java
@@ -8,25 +8,6 @@ import org.junit.Test;
 
 public class Tests {
 
-	private App test;
 	
-	@Before
-	public void setUp() throws Exception{
-		test = new App();
-	}
-	
-
-	@Test
-	public void test() {
-		Assert.assertFalse(test.containString("Filarmonica", "pastel"));
-		Assert.assertTrue(test.containString("Filarmonica", "monica"));
-		
-		Assert.assertEquals(0, test.comparatorInt(3, 3));
-		Assert.assertEquals(1, test.comparatorInt(4, 3));
-		Assert.assertEquals(-1, test.comparatorInt(3, 4));
-		
-		double result =28.27;
-		Assert.assertEquals(result, test.circleArea(3));
-	}
 
 }
