4763155f766183beb823b5ccbba847c1f473027e
jpdco-iscteiul <jpdco@iscte-iul.pt>
2020-03-26 14:05:42 +0000

    Projeto Hello Word para teste

---
+++ ESII-G28/src/test/java/ES1_2019_C2_66/ESII_G28/TestTest.java
@@ -0,0 +1,14 @@
+package ES1_2019_C2_66.ESII_G28;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ HelloWorld/src/main/java/ES1_2019_C2_66/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package ES1_2019_C2_66.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
